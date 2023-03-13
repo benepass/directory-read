d45b1209afab2e09578605fc65aecb12aee29f6b
Rodolfo Lottin Pereira <rodolfolottin1@gmail.com>
2023-03-13 15:46:47 -0300

    feat: First version of extractor lambda function

--- extraction/tests/e2e/test_app.py
+++ extraction/tests/e2e/test_app.py
@@ -1,4 +1,9 @@
-def test_dummy_function(client, make_cloudwatch_scheduled_event):
+def test_extract_function(
+    client,
+    freeze,
+    mocked_merge_roster_response,
+    make_cloudwatch_scheduled_event,
+):
     assert client.lambda_.invoke(
-        "dummy", make_cloudwatch_scheduled_event(client)
-    ).payload == {"ping": "pong"}
+        "extract", make_cloudwatch_scheduled_event(client)
+    ).payload == {"ok": True, "data": ""}
